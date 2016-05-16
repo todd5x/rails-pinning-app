aec2660b6289b0d557ff09c02d04ada4991022cd
Todd <todd.anthony.honeycutt@gmail.com>
2016-05-16 01:06:32 +0000

    Add some style

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,11 +1,17 @@
 class PinsController < ApplicationController
-  
+
   def index
     @pins = Pin.all
   end
-  
+
   def show
     @pin = Pin.find(params[:id])
   end
+
+  def show_by_name
+    @pin = Pin.find_by_slug(params[:slug])
+    render :show
+  end
+  #name-ruby-monk
   
 end
--- spec/controllers/pins_controller_spec.rb
+++ spec/controllers/pins_controller_spec.rb
@@ -1,4 +1,12 @@
 require 'spec_helper'
-RSpec.describe PinsController do
 
+RSpec.describe PinsController do
+ 
+    describe "GET index" do
+ 
+      it 'renders the index template' do
+      
+      end
+      
+    end
 end
