aeaed018adb45a86dafe455152ffbcbaf351466c
Todd <todd.anthony.honeycutt@gmail.com>
2016-05-19 07:30:24 +0000

    image add

--- db/migrate/20160519050244_add_attachment_image_to_pins.rb
+++ db/migrate/20160519050244_add_attachment_image_to_pins.rb
@@ -1,7 +1,7 @@
 class AddAttachmentImageToPins < ActiveRecord::Migration
   def self.up
     change_table :pins do |t|
-      t.attachment :image
+      t.attachment :image, :image_file_name
     end
   end
 
